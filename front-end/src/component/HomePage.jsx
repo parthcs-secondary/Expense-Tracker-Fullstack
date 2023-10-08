2d05fafba03ca8fcf8175ce844dd0e9191de931c
AmitGaikar00 <amitgaikar6025@gmail.com>
2023-10-08 09:59:22 +0530

    after register page will navigate to login page login submit will get jwt , jwt will be stored in localstorage navigate to home page get jwt from local if jwt present login , register won't show , logout will show

--- front-end/src/component/HomePage.jsx
+++ front-end/src/component/HomePage.jsx
@@ -1,18 +1,26 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+  const navigate = useNavigate()
+  const JwtToken = JSON.parse(localStorage.getItem("JWT"));
+  const handleLogout = (e) => {
+    e.preventDefault();
+    localStorage.clear();
+    navigate('/');
+  };
+
   return (
-    <div style={{display: "flex", flexDirection: "column"}}>
-        <span>Expense Tracker</span>
-        <Link to="/login">Login</Link>
-        <Link to="/register">Register</Link>
-        <Link to="/expense-form">Handle Expenses</Link>
-        <Link to="/show-expenses">View Expenses</Link>
-
-
+    <div style={{ display: "flex", flexDirection: "column" }}>
+      <span>Expense Tracker</span>
+      {!JwtToken && <Link to="/login">Login</Link>}
+      {JwtToken && <button onclick={handleLogout}>logout</button>}
+      {!JwtToken && <Link to="/register">Register</Link>}
+      <Link to="/expense-form">Handle Expenses</Link>
+      <Link to="/show-expenses">View Expenses</Link>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
